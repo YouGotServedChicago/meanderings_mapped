8629a4a709940de0b8635f5dd31f8f1f6e9848d6
Katherine Ude <Katie.Ude@gmail.com>
2015-12-08 13:46:49 -0600

    saving front end work

--- app.rb
+++ app.rb
@@ -2,5 +2,5 @@ require 'bundler'
 Bundler.require
 
 get '/' do
-  erb :login
+  erb :profile
 end
--- db/migration.sql
+++ db/migration.sql
@@ -2,8 +2,8 @@ CREATE DATABASE meandering;
 
 \c meandering
 
-CREATE TABLE ();
+CREATE TABLE users (id SERIAL PRIMARY KEY, user_name VARCHAR(30), password VARCHAR(255), date_of_birth DATE, city VARCHAR(255), state_province VARCHAR(255), country VARCHAR(255), bio TEXT);
 
-CREATE TABLE ();
+CREATE TABLE photos (id SERIAL PRIMARY KEY, photo TEXT, upload_time TIMESTAMP, user_id INTEGER REFERENCES users (id));
 
-CREATE TABLE ();
+CREATE TABLE entries (id SERIAL PRIMARY KEY, title TEXT, entry_date DATE, journal_snippet TEXT, user_id INTEGER REFERENCES users (id));
