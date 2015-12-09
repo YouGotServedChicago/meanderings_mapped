5eeb7595cd820e029b5cdc27fbdc5685351e7454
Katherine Ude <Katie.Ude@gmail.com>
2015-12-09 11:16:10 -0600

    saving minor updates

--- controllers/profile.rb
+++ controllers/profile.rb
@@ -4,21 +4,23 @@ class ProfilesController < ApplicationController
     erb :profile
   end
 
-  post '/create' do
-    p params
-    @users = User.new
-    @users.user_name = params[:profile_name]
-    @users.date_of_birth = params[:date_of_birth]
-    @users.city = params[:city]
-    @users.bio = params[:bio]
-    @users.save
-    erb :profile
-  end
-
   get '/create' do
-    @users = User.all
     erb :profile_create
   end
 
+  post '/create' do
+    puts "----------------"
+    puts params
+    puts "-----------------"
+    @profiles = Profile.new
+    @profiles.profile_name = params[:profile_name]
+    @profiles.date_of_birth = params[:date_of_birth]
+    @profiles.city = params[:city]
+    @profiles.bio = params[:bio]
+    @profiles.image = params[:image]
+    @profiles.base_64_image = params[:image_as_base64]
+    @profiles.save
+    erb :profile
+  end
 
 end
