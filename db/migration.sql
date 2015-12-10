bd5c3830f75949acad679f824e6db3da6e324172
Katherine Ude <Katie.Ude@gmail.com>
2015-12-10 12:10:17 -0600

    saving modified db table

--- db/migration.sql
+++ db/migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, user_name VARCHAR(30), user_email VAR
 
 CREATE TABLE profiles (id SERIAL PRIMARY KEY, profile_name VARCHAR(50), date_of_birth DATE, city VARCHAR(255), bio TEXT, image TEXT, base_64_image TEXT, user_id INTEGER);
 
-CREATE TABLE entries (id SERIAL PRIMARY KEY, title TEXT, entry_date DATE, journal_snippet TEXT, user_id INTEGER REFERENCES users);
+CREATE TABLE entries (id SERIAL PRIMARY KEY, title TEXT, entry_date DATE, journal_snippet TEXT, user_id INTEGER);
 
 
 INSERT INTO profiles(profile_name, user_id) VALUES ('jim', 1)
