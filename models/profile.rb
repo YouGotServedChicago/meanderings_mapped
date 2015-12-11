70bc46a86cf293fdbe19fc7a108c7b0d2a991b3f
Katherine Ude <Katie.Ude@gmail.com>
2015-12-10 19:55:58 -0600

    saving functionality before attempting to merge final versions

--- models/profile.rb
+++ models/profile.rb
@@ -1,4 +1,9 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
 
+  def get_users_age(birthdate)
+    return Time.now.to_s.split('-')[0].to_i - birthday.to_s.split('-')[0].to_i
+  end
+  #Profile.get_users_age(name_of_model.birthdate)
+
 end
